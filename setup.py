1735ea3bc9627e7f440b227f8f1a4178eb614033
Adam Jakubek <ajakubek@gmail.com>
2013-03-03 17:45:56 +0100

    Fix author list in package metadata

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='pyllist',
       description='Linked list data structures (in pure Python)',
       long_description=open('README').read(),
       author=u'Rafał Gałczyński <rafal.galczynski@gmail.com>, '
-             u'Adam Jakubek <ajakubek@gmail.com>'
+             u'Adam Jakubek <ajakubek@gmail.com>, '
              u'Oleksandr Pryymak',
       version=VERSION,
       url='https://github.com/rgsoda/pypy-llist',
